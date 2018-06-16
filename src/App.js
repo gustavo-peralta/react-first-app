5523e111c0302cd06e8db9df4d71ac1bc5e3f9ec
guper08@gmail.com <guper08@gmail.com>
2018-06-16 12:45:22 +0200

    -

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 import { todos } from './todos.json';
+import TodoForm from './components/TodoForm';
+
 //console.log(todos);
 
 class App extends Component {
@@ -13,8 +15,31 @@ class App extends Component {
     this.state = {
       todos: todos
     }
+
+     // Para que el metodo handleInput no pierda el Scope
+      // Con esto decimos que este this.handleInput pertenece a este componente.
+      this.handleAddTodo = this.handleAddTodo.bind(this);
   }
   
+  handleAddTodo(todo){
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
+
+  removeTodo(index){
+
+    // Como es un evento propio del navegador hay que agregarle la palabra window por delante
+    if(window.confirm('Are you sure you want to delete i')){
+      this.setState({
+        // Filter recorre uno a uno los elementos y si un dato no cumple con la condición no lo agrega al nuevo arreglo
+        todos: this.state.todos.filter((el, i) => {
+          return i !== index
+        })
+      })
+    }
+   
+  }
   
   render() {
 
@@ -23,19 +48,27 @@ class App extends Component {
     // Las tareas son un arreglo y lo puedo recorrer con el metodo map() de JS
     const todos = this.state.todos.map((todo, i) => {
       return (
-        <div className="col-md-4">
+        <div className="col-md-4" key={i}>
           <div className="card mt-4">
-          <div className="card-header">
-            <h3>{ todo.title }</h3>
-            <span className="badge badge-pill badge-danger ml-2">
-              { todo.priority }
-            </span>
+            <div className="card-header">
+              <h3>{ todo.title }</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                { todo.priority }
+              </span>
+            </div>
+            <div className="card-body"> 
+              <p>{ todo.description }</p>
+              <p><mark>{ todo.responsible }</mark></p>
+            </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.removeTodo.bind(this, i)} // No solo llamo al metodo sino que lo bindo a esta clase para que sepa que es sullo
+              >
+              Delete
+              </button>
+            </div>
           </div>
-          <div className="card-body"> 
-            <p>{ todo.description }</p>
-            <p><mark>{ todo.responsible }</mark></p>
-          </div>
-        </div>
         </div>
       )
     })
@@ -54,12 +87,19 @@ class App extends Component {
 
         <div className="container">
           <div className="row mt-4">
-          { todos }  
-          </div>  
-        </div>
-        
 
-        <img src={logo} className="App-logo" alt="logo" />
+            <div className="col-md-4 text-center">
+                <img src={logo} className="App-logo" alt="logo" />
+              <TodoForm onAddTodo={this.handleAddTodo} />
+            </div>
+
+            <div className="col-md-8">
+              <div className="row">
+                {todos}
+              </div>
+            </div>
+          </div>
+        </div>
           
       </div>
     );
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,5 +1,95 @@
 import React, { Component } from 'react';
 
 class TodoForm extends Component{
+    constructor(){
+        super();
+        this.state = {
+            title: '',
+            responsible: '',
+            description: '',
+            priority: 'low'
+        };
+
+        // Para que el metodo handleInput no pierda el Scope
+        // Con esto decimos que este this.handleInput pertenece a este componente.
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInput(e){
+        console.log(e.target.value, e.target.name);
+
+        // Deestructuring javascript
+        const { value, name } = e.target;
+
+        //Metodo usado para poder modificar el estado del componente
+        this.setState({
+            [name] : value
+        })
+
+        // Ahora quiero enviar los datos sin necesidad de refrescar el navegador
+
+        console.log(this.state);
+    }
+
+    handleSubmit(e){
+        // Evento que evita refrescar la pantalla
+        e.preventDefault();
+        this.props.onAddTodo(this.state);
+        console.log('sending data....');
+
+    }
+
+    render(){
+        return(
+            <div className="card">
+                <form className="card-body" onSubmit={this.handleSubmit}> 
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            name="title"
+                            onChange={this.handleInput}
+                            className="form-control"
+                            placeholder="Title"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            name="responsible"
+                            onChange={this.handleInput}
+                            className="form-control"
+                            placeholder="Responsible"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            name="description"
+                            onChange={this.handleInput}
+                            className="form-control"
+                            placeholder="Description"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <select                 
+                            name="priority"
+                            className="form-control"         
+                            onChange={this.handleInput}
+                        >
+                        <option>low</option>
+                        <option>medium</option>
+                        <option>high</option>
+                        </select>
+                    </div>
+                    <button type="submit" className="btn btn-primary">
+                        Save
+                    </button>
+                </form>    
+            </div> 
+        )
+    }
 
 }
+
+export default TodoForm;
